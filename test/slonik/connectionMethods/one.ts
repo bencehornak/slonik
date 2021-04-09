4838d6bf03e91d733f4aa756ea0907934e21abb1
Bence Hornák <bence.hornak@gmail.com>
2021-04-09 11:14:53 +0200

    test: check allowed return types

--- test/slonik/connectionMethods/one.ts
+++ test/slonik/connectionMethods/one.ts
@@ -1,7 +1,6 @@
 import test from 'ava';
 import {
-  DataIntegrityError,
-  NotFoundError,
+  DataIntegrityError, NotFoundError,
 } from '../../../src/errors';
 import {
   createSqlTag,
@@ -59,4 +58,41 @@ test('throws an error if more than one row is returned', async (t) => {
   const error = await t.throwsAsync(pool.one(sql`SELECT 1`));
 
   t.true(error instanceof DataIntegrityError);
+});
+
+test('typing is correct', async (t) => {
+  type Foo = {
+    date: Date,
+    number: number,
+    string: string,
+  };
+  const pool = createPool();
+  const date = new Date('2000-01-01T00:00:00.000Z');
+  const number = 1;
+  const string = 'a';
+
+  pool.querySpy.returns({
+    rows: [
+      {
+        date,
+        number,
+        string,
+      },
+    ],
+  });
+
+  t.deepEqual(
+    await pool.one<Foo>(sql`
+      SELECT
+        '2000-01-01T00:00:00.000Z'::timestamp AS date,
+        1 AS number,
+        'a' AS string
+    `),
+    {
+      // a timestamp typeParser is assumed
+      date,
+      number,
+      string,
+    } as Foo,
+  );
 });
